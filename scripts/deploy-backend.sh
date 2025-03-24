053829db2fa478e335e8f933916bd42fd141ea81
Pavel Klochkov <paul.klochkov@gmail.com>
2025-03-24 13:47:49 +0100

    Add deploy script

---
+++ scripts/deploy-backend.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+[[ -f .env ]] && export $(grep -v '^#' .env | xargs)
+
+: "${DEPLOY_USER:?Missing DEPLOY_USER}"
+: "${DEPLOY_HOST:?Missing DEPLOY_HOST}"
+: "${DEPLOY_PATH:?Missing DEPLOY_PATH}"
+
+ssh -tt "$DEPLOY_USER@$DEPLOY_HOST" "su - nodejs -c \"bash -lc '
+  export NVM_DIR=\$HOME/.nvm
+  [ -s \\\"\$NVM_DIR/nvm.sh\\\" ] && . \\\"\$NVM_DIR/nvm.sh\\\"
+  export PATH=\\\"/home/nodejs/.nvm/versions/node/v20.18.2/bin:\\\$PATH\\\"
+
+  echo
+  echo \\\"🔄 Pulling latest code...\\\"
+  echo \\\"---------------------------\\\"
+  cd $DEPLOY_PATH
+  git pull
+
+  echo
+  echo \\\"📦 Installing dependencies...\\\"
+  echo \\\"------------------------------\\\"
+  npm ci
+
+  echo
+  echo \\\"🔧 Building admin panel...\\\"
+  echo \\\"---------------------------\\\"
+  npm run build --workspace=backend
+
+  echo
+  echo \\\"🚀 Restarting server with PM2...\\\"
+  echo \\\"--------------------------------\\\"
+  pm2 restart ./packages/backend/ecosystem.config.js
+
+  echo
+  echo \\\"✅ Done!\\\"
+'\" && exit"
